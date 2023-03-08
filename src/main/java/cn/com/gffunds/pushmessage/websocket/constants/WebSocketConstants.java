e7a3a7db84bea346937870d24cfd35a473fb2fef
s-hezc <454785892@qq.com>
2023-03-08 14:04:17 +0800

    feat:调整命令响应和消息响应的msgType

--- src/main/java/cn/com/gffunds/pushmessage/websocket/constants/WebSocketConstants.java
+++ src/main/java/cn/com/gffunds/pushmessage/websocket/constants/WebSocketConstants.java
@@ -13,9 +13,9 @@ public class WebSocketConstants {
     /** 分割符 */
     public static final String SEPARATOR = ":";
     /** 消息类型  */
-    public static final String MSG_TYPE_NORMAL = "0";
+    public static final String MSG_TYPE_NORMAL = "biz";
     /** 命令类型 */
-    public static final String MSG_TYPE_COMMAND = "1";
+    public static final String MSG_TYPE_COMMAND = "command";
     /** 合法 */
     public static final AtomicBoolean VALID = new AtomicBoolean(true);
     /** 不合法 */
