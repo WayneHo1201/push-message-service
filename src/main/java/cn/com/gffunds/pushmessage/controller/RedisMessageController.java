de699de535d2bba62003f2c36ed5238d7376b6e9
s-hezc <454785892@qq.com>
2023-03-09 10:24:11 +0800

    refactor:去除测试用客户端

--- src/main/java/cn/com/gffunds/pushmessage/controller/RedisMessageController.java
+++ src/main/java/cn/com/gffunds/pushmessage/controller/RedisMessageController.java
@@ -1,11 +1,8 @@
 package cn.com.gffunds.pushmessage.controller;
 
 import cn.com.gffunds.commons.json.JacksonUtil;
-import cn.com.gffunds.pushmessage.client.WebSocketClient;
-import cn.com.gffunds.pushmessage.websocket.entity.MessageRequest;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -47,18 +44,6 @@ public class RedisMessageController {
     public String sendIpm(@RequestBody JsonNode jsonNode) {
         String channel = jsonNode.get("channel").asText();
         ipmRedisTemplate.convertAndSend(channel, JacksonUtil.toJson(jsonNode.get("data")));
-        return "success";
-    }
-
-    /**
-     * 发送订阅退订信息
-     */
-    @Autowired
-    private WebSocketClient webSocketClient;
-
-    @PostMapping("/send")
-    public String send(@RequestBody MessageRequest request) {
-        webSocketClient.send(JacksonUtil.toJson(request));
         return "success";
     }
 }
